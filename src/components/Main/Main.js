7cecca5f24368a2b7680e7a5e3a79088db7867c2
IRUBAKAI <vinn.gfif@gmail.com>
2021-11-13 16:14:38 +0100

    added folder Form + helpers

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from "react";
 import styles from "./Main.module.css";
 import stylesJokeCard from "../JokeCard/JokeCard.module.css";
 import stylesFavourite from "../JokeCard/FavouriteList.module.css";
-import { CategorieBtn } from "../JokeCard/";
 import { JokeCard } from "../JokeCard/";
-import { FavouriteList } from "../JokeCard/";
 import { Pagination } from "../Pagination";
 import { favouriteMenu } from "./Icons";
+import Form from "./Form";
+import {
+  fetchCategories,
+  handleAddToLocalStorage,
+  handleOnClickRemove,
+} from "./helpers";
 
 function Main() {
   /// Pagination states
@@ -18,70 +22,7 @@ function Main() {
   const [jokes, setJokes] = useState([]);
   const [favourites, setFavourites] = useState([]);
   const [statusBurgerMenu, setStatusBurgerMenu] = useState("unActive");
-
   const [categories, setCategories] = useState([]);
-  const [categorie, setCategorie] = useState("");
-  const [search, setSearch] = useState("");
-  const [errorSearch, setErrorSearch] = useState("");
-
-  const [checkedRadio, setCheckedRadio] = useState("random");
-
-  /// LOCALSTORAGE Buttons
-
-  function handleAddToLocalStorage(joke) {
-    const newFavouriteList = [...favourites, joke];
-    const saveToLocalStorage = (joke) => {
-      localStorage.setItem("joke-to-favourite", JSON.stringify(joke));
-    };
-    setFavourites(newFavouriteList);
-    saveToLocalStorage(newFavouriteList);
-  }
-
-  function handleOnClickRemove(joke) {
-    const newFavouriteList = favourites.filter((favourite) => {
-      return favourite.id !== joke.id;
-    });
-    console.log(newFavouriteList);
-    const saveToLocalStorage = (joke) => {
-      localStorage.setItem("joke-to-favourite", JSON.stringify(joke));
-    };
-    setFavourites(newFavouriteList);
-    saveToLocalStorage(newFavouriteList);
-  }
-
-  /// FETCH REQUEST
-
-  function fetchCategories() {
-    fetch("https://api.chucknorris.io/jokes/categories")
-      .then((res) => res.json())
-      .then((data) => setCategories(data));
-  }
-
-  const handleRandomJokeAdd = () => {
-    if (checkedRadio === "random") {
-      fetch("https://api.chucknorris.io/jokes/random")
-        .then((res) => res.json())
-        .then((data) => setJokes([data]))
-        .catch((err) => console.log(err));
-      setCurrentPage(1);
-    }
-    if (checkedRadio === "categorie") {
-      fetch(`https://api.chucknorris.io/jokes/random?category=${categorie}`)
-        .then((res) => res.json())
-        .then((data) => setJokes([data]))
-        .catch((err) => console.log(err));
-      setCurrentPage(1);
-    }
-    if (checkedRadio === "search") {
-      if (search.length >= 3) {
-        fetch(`https://api.chucknorris.io/jokes/search?query=${search}`)
-          .then((res) => res.json())
-          .then((data) => setJokes(data.result))
-          .catch((err) => console.log(err));
-        setErrorSearch(null);
-      } else setErrorSearch("You need 3 letters for search");
-    }
-  };
 
   // useEffects
 
@@ -90,8 +31,8 @@ function Main() {
       JSON.parse(localStorage.getItem("joke-to-favourite")) || [];
     setFavourites(jokeFavourites);
 
-    fetchCategories();
-  }, [setFavourites]);
+    fetchCategories(setCategories);
+  }, []);
 
   //BURGER MENU CHANGE
 
@@ -119,85 +60,12 @@ function Main() {
 
         {/* FORM */}
 
-        <form className={styles.checkbox_block}>
-          <label>
-            <input
-              type="radio"
-              name="name1"
-              onChange={() => {
-                setCheckedRadio("random");
-              }}
-            />
-            Random
-          </label>
-
-          <label>
-            <input
-              type="radio"
-              name="name1"
-              onChange={() => {
-                setCheckedRadio("categorie");
-              }}
-            />
-            From categories
-          </label>
-
-          <div
-            className={
-              checkedRadio === "categorie"
-                ? styles.categorie_btns_block
-                : styles.unActive
-            }
-          >
-            {categories.map((categorie) => (
-              <CategorieBtn
-                categorie={categorie}
-                setCategorie={setCategorie}
-                categories={categories}
-              />
-            ))}
-          </div>
-
-          <label>
-            <input
-              type="radio"
-              name="name1"
-              onChange={() => {
-                setCheckedRadio("search");
-              }}
-            />
-            Search
-          </label>
-
-          {checkedRadio === "search" ? (
-            <>
-              <input
-                type="text"
-                className={styles.searchInput}
-                placeholder="Free text search..."
-                onChange={(event) => setSearch(event.target.value)}
-              />
-              <p className={styles.errorSearch}>{errorSearch}</p>
-              <h1
-                className={
-                  currentJokes.length === 0 ? styles.active : styles.unActive
-                }
-              >
-                Nothing is here &#129488;
-              </h1>
-            </>
-          ) : null}
-
-          <input
-            type="submit"
-            className={styles.getJoke_btn}
-            onClick={(event) => {
-              event.preventDefault();
-              handleRandomJokeAdd(event);
-            }}
-            value="Get a joke"
-          />
-        </form>
+        <Form
+          categories={categories}
+          currentJokes={currentJokes}
+          setJokes={setJokes}
+          setCurrentPage={setCurrentPage}
+        />
 
         {/* RENDER JOKES */}
 
@@ -208,8 +76,8 @@ function Main() {
             );
             function storageButtonsAddNRemove() {
               if (!isFavourite) {
-                handleAddToLocalStorage(joke);
-              } else handleOnClickRemove(joke);
+                handleAddToLocalStorage(joke, favourites, setFavourites);
+              } else handleOnClickRemove(joke, favourites, setFavourites);
             }
 
             return (
@@ -235,10 +103,14 @@ function Main() {
       <div className={stylesFavourite.favourite_block}>
         <h1>Favourite</h1>
         {favourites.map((favourite) => {
+          function storageButtonRemove() {
+            handleOnClickRemove(favourite, favourites, setFavourites);
+          }
           return (
-            <FavouriteList
-              favourites={favourite}
-              handleOnClickRemove={handleOnClickRemove}
+            <JokeCard
+              joke={favourite}
+              styles={stylesFavourite}
+              storageButtons={storageButtonRemove}
             />
           );
         })}
@@ -266,10 +138,14 @@ function Main() {
         }
       >
         {favourites.map((favourite) => {
+          function storageButtonRemove() {
+            handleOnClickRemove(favourite);
+          }
           return (
-            <FavouriteList
-              favourites={favourite}
-              handleOnClickRemove={handleOnClickRemove}
+            <JokeCard
+              joke={favourite}
+              styles={stylesFavourite}
+              storageButtons={storageButtonRemove}
             />
           );
         })}
